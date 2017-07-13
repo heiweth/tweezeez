7dd3d4809f433e2a7526cf77ca86cd606cc9ffe2
heiweth <sanja712@yahoo.com>
2017-07-13 08:41:24 +0200

    Create timeline.py

---
+++ timeline.py
@@ -0,0 +1,8 @@
+from auth import *
+
+# Using the API object to get tweets from your timeline, and storing it in a variable called public_tweets
+public_tweets = api.home_timeline()
+# foreach through all tweets pulled
+for tweet in public_tweets:
+   # printing the text stored inside the tweet object
+   print tweet.text
