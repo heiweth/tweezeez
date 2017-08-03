12f6eabb9a87f978601dd01f836e258bf065cd5a
heiweth <sanja712@yahoo.com>
2017-08-03 09:29:14 +0200

    Update spijunaza_timeline.py

--- spijunaza_timeline.py
+++ spijunaza_timeline.py
@@ -2,14 +2,18 @@
 api = tweepy.API(auth)
 
 # The Twitter user who we want to get tweets from
-name = "Savićeva"
+name = "Precarobna"
 # Number of tweets to pull
-tweetCount = 20
+tweetCount = 100
+query="ultimatum"
 
 # Calling the user_timeline function with our parameters
 results = api.user_timeline(id=name, count=tweetCount)
+res = api.search(q=query)
+
 
 # foreach through all tweets pulled
 for tweet in results:
+   
    # printing the text stored inside the tweet object
    print tweet.text
